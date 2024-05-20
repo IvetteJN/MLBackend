e2a88234d66d422dd89019ba69f7072dca42cc35
Ivette Nobilta <ivette.nobilta@hotmail.comgit config --global user.name Ivette>
2024-05-20 18:07:31 -0300

    nuevos modelos relacionales y modelos django

--- myproject/ecommerce/admin.py
+++ myproject/ecommerce/admin.py
@@ -1,23 +1,56 @@
 from django.contrib import admin
-from .models import Libro, Categoria, Pedido, ItemPedido, Usuario
+from .models import Libro, Categoria, Pedido, DetallePedido, UsuarioCliente, Autor, Direccion, HistorialPedido, Resena, LibroAutor, UsuarioAdministrador, UsuarioClienteAdministrador, LibroAdministrador
 
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('categoria',)
 
-class LibroAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'autor', 'portada', 'descripcion', 'precio', 'stock')
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido')
 
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'correo_electronico', 'contrasena', 'direccion_envio')
+class LibroAutorInline(admin.TabularInline):
+    model = LibroAutor
+
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'display_autores', 'precio', 'stock', 'categoria')
+    inlines = [LibroAutorInline]
+
+    def display_autores(self, obj):
+        return ', '.join([autor.nombre_completo() for autor in obj.autor.all()])
+
+    display_autores.short_description = 'Autor(es)'
+
+class UsuarioClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo_electronico', 'contrasena')
+
+class UsuarioAdministradorAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'contrasena')
 
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ('__str__',)
+    list_display = ('id_pedido', 'cliente', 'estado_pedido', 'fecha_pedido')
 
-class ItemPedidoAdmin(admin.ModelAdmin):
-    list_display = ('libro', 'pedido', 'cantidad')
+class DetallePedidoAdmin(admin.ModelAdmin):
+    list_display = ('id_detalle', 'pedido', 'libro', 'cantidad', 'precio_unitario', 'precio_total')
 
-admin.site.register(Libro, LibroAdmin)
+class HistorialPedidoAdmin(admin.ModelAdmin):
+    list_display = ('id_historial', 'pedido', 'estado_pedido', 'fecha_cambio')
+
+class DireccionAdmin(admin.ModelAdmin):
+    list_display = ('id_direccion', 'usuario', 'direccion', 'ciudad', 'estado', 'codigo_postal', 'pais')
+
+class ResenaAdmin(admin.ModelAdmin):
+    list_display = ('id_resena', 'libro', 'usuario', 'calificacion', 'fecha_resena')
+
+class LibroAdministradorAdmin(admin.ModelAdmin):
+    list_display = ('administrador', 'libro')
+
 admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libro, LibroAdmin)
+admin.site.register(UsuarioCliente, UsuarioClienteAdmin)
+admin.site.register(UsuarioAdministrador, UsuarioAdministradorAdmin)
 admin.site.register(Pedido, PedidoAdmin)
-admin.site.register(ItemPedido, ItemPedidoAdmin)
-admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(DetallePedido, DetallePedidoAdmin)
+admin.site.register(HistorialPedido, HistorialPedidoAdmin)
+admin.site.register(Direccion, DireccionAdmin)
+admin.site.register(Resena, ResenaAdmin)
+admin.site.register(LibroAdministrador, LibroAdministradorAdmin)
--- myproject/ecommerce/models.py
+++ myproject/ecommerce/models.py
@@ -1,4 +1,68 @@
 from django.db import models
+from django.utils import timezone
+
+# usuarios
+
+class UsuarioCliente(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    correo_electronico = models.EmailField(unique=True)
+    contrasena = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+
+class UsuarioAdministrador(models.Model):
+    id_admin = models.AutoField(primary_key=True)
+    usuario = models.CharField(max_length=255, unique=True)
+    contrasena = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.usuario
+
+# direccion
+
+class Direccion(models.Model):
+    id_direccion = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(UsuarioCliente, on_delete=models.CASCADE)
+    direccion = models.TextField()
+    ciudad = models.CharField(max_length=255)
+    estado = models.CharField(max_length=255)
+    codigo_postal = models.CharField(max_length=20)
+    pais = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.direccion}, {self.ciudad}, {self.pais}'
+
+#usuario_administrador - usuario_cliente
+
+class UsuarioClienteAdministrador(models.Model):
+    administrador = models.ForeignKey(UsuarioAdministrador, on_delete=models.CASCADE)
+    usuario_cliente = models.ForeignKey(UsuarioCliente, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('administrador', 'usuario_cliente')
+
+    def __str__(self):
+        return f'{self.administrador.usuario} - {self.usuario_cliente.nombre}'
+
+# libro, categoria, autor, libro-autor
+
+class Libro(models.Model):
+    id_libro = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+    autor = models.ManyToManyField('Autor', through='LibroAutor')
+    
+    class Meta:
+        db_table = 'libro'
+        verbose_name = 'Libro'
+        verbose_name_plural = 'Libros'
+
+    def __str__(self):
+        return self.titulo
 
 class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True)
@@ -10,48 +74,100 @@ class Categoria(models.Model):
         verbose_name_plural = 'Categorías'
 
     def __str__(self):
-        return self.categoria
+        return self.nombre
 
-class Libro(models.Model):
-    id_libro = models.AutoField(primary_key=True)
-    titulo = models.CharField(max_length=255)
-    autor = models.CharField(max_length=255)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    portada = models.ImageField(upload_to='portadas/')
-    descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+class Autor(models.Model):
+    id_autor = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
 
     class Meta:
-        db_table = 'libro'
-        verbose_name = 'Libro'
-        verbose_name_plural = 'Libros'
+        db_table = 'autor'
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
 
     def __str__(self):
-        return self.titulo
+        return f'{self.nombre} {self.apellido}'
 
-class Usuario(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=255)
-    correo_electronico = models.EmailField(unique=True)
-    contrasena = models.CharField(max_length=255)
-    direccion_envio = models.TextField()
+class LibroAutor(models.Model):
+    id = models.AutoField(primary_key=True)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'libro_autor'
+        verbose_name = 'Libro-Autor'
+        verbose_name_plural = 'Libros-Autores'
 
     def __str__(self):
-        return self.nombre
+        return f'{self.libro.titulo} - {self.autor.nombre} {self.autor.apellido}'
+
+# reseña
+
+class Resena(models.Model):
+    id_resena = models.AutoField(primary_key=True)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(UsuarioCliente, on_delete=models.CASCADE)
+    comentario = models.TextField()
+    calificacion = models.IntegerField()
+    fecha_resena = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.libro.titulo} - {self.usuario.nombre}'
+
+
+# usuario_administrador - libro
+
+class LibroAdministrador(models.Model):
+    administrador = models.ForeignKey(UsuarioAdministrador, on_delete=models.CASCADE)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('administrador', 'libro')
+
+    def __str__(self):
+        return f'{self.administrador.usuario} - {self.libro.titulo}'
+
+# pedido, detalle_pedido, historial pedido
+
+from django.db import models
+from .models import Libro, UsuarioCliente
 
 class Pedido(models.Model):
     id_pedido = models.AutoField(primary_key=True)
-    productos = models.ManyToManyField(Libro, through='ItemPedido')
+    cliente = models.ForeignKey(UsuarioCliente, on_delete=models.CASCADE, default=1) 
+    estado_pedido = models.CharField(max_length=100, default='Pendiente')
+    fecha_pedido = models.DateTimeField(default=timezone.now, editable=False)
 
-    def total_pedido(self):
-        total = 0
-        for item in self.itempedido_set.all():
-            total += item.cantidad * item.libro.precio
-        return total
+    class Meta:
+        db_table = 'pedido'
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
 
-class ItemPedido(models.Model):
-    id_itempedido = models.AutoField(primary_key=True)
-    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'Pedido {self.id_pedido} - {self.cliente.nombre}'
+
+class DetallePedido(models.Model):
+    id_detalle = models.AutoField(primary_key=True)
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
     cantidad = models.IntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'detalle_pedido'
+        verbose_name = 'Detalle de Pedido'
+        verbose_name_plural = 'Detalles de Pedidos'
+
+    def __str__(self):
+        return f'Detalle - Pedido {self.pedido.id_pedido} - {self.libro.titulo}'
+
+class HistorialPedido(models.Model):
+    id_historial = models.AutoField(primary_key=True)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    estado_pedido = models.CharField(max_length=100, default='Pendiente')
+    fecha_cambio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.pedido.id_pedido} - {self.estado_pedido}'
