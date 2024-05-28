827c6bf713b5584e4cfe4750461a0bc69c20f5eb
Ivette Nobilta <ivette.nobilta@hotmail.comgit config --global user.name Ivette>
2024-05-28 18:25:05 -0300

    stage files

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mercadolibro',
+        'NAME': 'mercado_libro',
         'USER': 'root',
-        'PASSWORD': 'Argentina15', #Introducir contraseña
+        'PASSWORD': 'Ismael07', #Introducir contraseña
         'HOST': 'localhost',  
-        'PORT': '3306',     
+        'PORT': '3307',     
     }
 }
 
