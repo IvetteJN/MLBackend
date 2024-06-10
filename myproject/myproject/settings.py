e51fd61e65ed588333e12ca20e1c10df179b765a
Ivette Nobilta <ivette.nobilta@hotmail.comgit config --global user.name Ivette>
2024-06-10 15:27:18 -0300

    revisiones

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -140,3 +140,12 @@ DEBUG = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
